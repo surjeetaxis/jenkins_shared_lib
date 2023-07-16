dac9dc016388eaa156a78733fc81d4ad7d579127
surjeetaxis <surjeet@axisrooms.com>
2023-07-16 21:56:57 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,8 @@
 def call(String project, String ImageTag, String dockerHubUser){
     
     sh """
-     docker image build -t ${dockerHubUser}/${project} . 
-     docker image tag ${dockerHubUser}/${project} ${dockerHubUser}/${project}:${ImageTag}
-     docker image tag ${dockerHubUser}/${project} ${dockerHubUser}/${project}:latest
+     docker image build -t ${project} . 
+     docker image tag ${project} ${dockerHubUser}/${project}:${ImageTag}
     """
 }
 
